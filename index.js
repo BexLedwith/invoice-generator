e0a466c62235b81089c99c266441da4440b7994a
Bex Akeakamai <bex.akeakamai@gmail.com>
2022-02-14 16:26:38 -0500

    array for services

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const themeToggle = document.getElementById("toggle");
 const body = document.getElementById("body");
+const car = document.getElementById("car");
+const lawn = document.getElementById("lawn");
+const weeds = document.getElementById("weeds");
+
+const servicesArr = [];
 
 // toggle dark mode
 themeToggle.addEventListener("click", function () {
   body.classList.contains("dark")
     ? body.classList.remove("dark")
     : body.classList.add("dark");
+});
+
+// add services to array
+const addServices = (service) => {
+  servicesArr.push(service);
+};
+
+car.addEventListener("click", function () {
+  addServices("car");
+  console.log(servicesArr);
 });
